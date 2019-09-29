1d961ea94254ab1cccba707d5e68f59ce72888f4
nicolechoong <37162961+nicolechoong@users.noreply.github.com>
2019-09-29 10:38:55 +0800

    Create modifying-oop.py

---
+++ modifying-oop.py
@@ -0,0 +1,50 @@
+#1. Modify cmp so that Aces are ranked higher than Kings.
+
+class Card:
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    ranks = ["narf", "Ace", "2", "3", "4", "5", "6", "7",
+             "8", "9", "10", "Jack", "Queen", "King"]
+
+    def __init__(self, suit=0, rank=0):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return (self.ranks[self.rank] + " of " + self.suits[self.suit])
+
+    def cmp(self, other):
+        # Check the suits
+        if self.suit > other.suit: return 1
+        if self.suit < other.suit: return -1
+        # Suits are the same... check ranks
+        if self.rank == 1: return 1
+        if other.rank == 1: return -1
+        if self.rank > other.rank: return 1
+        if self.rank < other.rank: return -1
+        # Ranks are the same... it's a tie
+        return 0
+
+    def __eq__(self, other):
+        return self.cmp(other) == 0
+
+    def __le__(self, other):
+        return self.cmp(other) <= 0
+
+    def __ge__(self, other):
+        return self.cmp(other) >= 0
+
+    def __gt__(self, other):
+        return self.cmp(other) > 0
+
+    def __lt__(self, other):
+        return self.cmp(other) < 0
+
+    def __ne__(self, other):
+        return self.cmp(other) != 0
+
+card1 = Card(1, 1)
+card2 = Card(1, 3)
+card3 = Card(1, 13)
+
+print(card1.cmp(card3))
+print(card2.cmp(card3))

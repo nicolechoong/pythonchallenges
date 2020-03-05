8a1e16a4bf689786e990896b609e352bf177bd74
nicolechoong <37162961+nicolechoong@users.noreply.github.com>
2020-03-05 11:05:55 +0800

    Update pickley.py

--- pickley.py
+++ pickley.py
@@ -1,12 +1,16 @@
 import pickle
 
-example = [1,1,1,1]
+example = {"0000":"0", "0001":"1", "0010":"2", "0011":"3", "0100":"4", "0101":"5", "0110":"6", "0111":"7", "1000":"8", "1001":"9", "1010":"A", "1011":"B", "1100":"C", "1101":"D", "1110":"E", "1111":"F"}
 
 pickle_out = open("hello.pickle","wb")
 pickle.dump(example, pickle_out)
 pickle_out.close()
 
-pickle_in = open("hello.pickle","rb")
-example_list = pickle.load(pickle_in)
+def convert(binary):
+    pickle_in = open("hello.pickle","rb")
+    translate = pickle.load(pickle_in)
+    chunks = binary.split()
+    for i in chunks:
+        print(translate[i], end = " ")
 
-print(example_list)
+convert("0110 1101 0101")
